3f123c1e69f21da43a68e8021cb0c15790f9a1df
Nebz <28622481+NebzHB@users.noreply.github.com>
2024-01-04 11:25:36 +0100

    Update install_apt.sh

--- resources/install_apt.sh
+++ resources/install_apt.sh
@@ -4,6 +4,8 @@
 BASEDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 wget https://raw.githubusercontent.com/NebzHB/dependance.lib/master/dependance.lib -O $BASEDIR/dependance.lib &>/dev/null
 PLUGIN=$(basename "$(realpath $BASEDIR/..)")
+COLOR=1
+TIMED=1
 . ${BASEDIR}/dependance.lib
 ##################################################################
 wget https://raw.githubusercontent.com/NebzHB/nodejs_install/main/install_nodejs.sh -O $BASEDIR/install_nodejs.sh &>/dev/null
