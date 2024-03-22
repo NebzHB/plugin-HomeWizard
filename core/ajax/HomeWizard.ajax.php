13451845db10f2655f5157515dbc7188e8efbc7f
Nebz <28622481+NebzHB@users.noreply.github.com>
2024-03-22 11:53:32 +0100

    Update HomeWizard.ajax.php

--- core/ajax/HomeWizard.ajax.php
+++ core/ajax/HomeWizard.ajax.php
@@ -31,60 +31,7 @@ try {
 		if($daemonState['state'] != 'ok') {
 			ajax::error(__("Le démon n'est pas démarré", __FILE__));
 		}
-	}
-	
-	if (init('action') == 'pair') {
-		$request=[
-			'id'=>init('id'),
-			'address'=>init('address'),
-			'port'=>init('port'),
-			'pin'=>init('pin'),
-			'typeId'=>init('typeId'),
-			'name'=>init('name'),
-			'pairMethod'=>init('pairMethod')
-		];
-		log::add('HomeWizard','info',__("Demande d'appairage... : ", __FILE__).http_build_query($request));
-		$ret=HomeWizard::hkExecute('pair',$request);
-		log::add('HomeWizard','info',__("Appairage:", __FILE__).$ret['result']);
-		ajax::success($ret);
-	} elseif (init('action') == 'prePair') {
-		$request=[
-			'id'=>init('id'),
-			'address'=>init('address'),
-			'port'=>init('port'),
-			'typeId'=>init('typeId'),
-			'name'=>init('name'),
-			'pairMethod'=>init('pairMethod')
-		];
-		log::add('HomeWizard','info',__("Demande de pré-appairage... : ", __FILE__).http_build_query($request));
-		$ret=HomeWizard::hkExecute('prePair',$request);
-		log::add('HomeWizard','info',__("Pré-appairage:", __FILE__).$ret['result']);
-		ajax::success($ret);
-	} elseif (init('action') == 'postPair') {
-		$request=[
-			'id'=>init('id'),
-			'pin'=>init('pin')
-		];
-		log::add('HomeWizard','info',__("Demande de post-appairage... : ", __FILE__).http_build_query($request));
-		$ret=HomeWizard::hkExecute('postPair',$request);
-		log::add('HomeWizard','info',__("Post-appairage:", __FILE__).$ret['result']);
-		ajax::success($ret);
-	} elseif (init('action') == 'unpair') {
-		log::add('HomeWizard','info',__("Demande de désappairage : ", __FILE__).init('id'));
-		$ret=HomeWizard::hkExecute('unPair',['id'=>init('id')]);
-		log::add('HomeWizard','info',__("Désappairage:", __FILE__).$ret['result']);
-		ajax::success($ret);
-	} elseif (init('action') == 'removeFromPairings') {
-		log::add('HomeWizard','info',__("Demande de suppression d'appairage :", __FILE__).json_encode(init('removeList')));
-		$ret=HomeWizard::removePairing(init('removeList'));
-		ajax::success($ret);
-	} elseif (init('action') == 'refresh') {
-		log::add('HomeWizard','info',__("Refresh d'un accessoire :", __FILE__).json_encode(init('id')));
-		$eq=HomeWizard::byLogicalId(init('id'),'hkControl');
-		$cmd=$eq->getCmd(null,'refresh');
-		$cmd->execCmd();
-		ajax::success(['result'=>'ok']);
-	} elseif (init('action') ==  'reinstallNodeJS') {
+
 		$ret = HomeWizard::reinstallNodeJS();
 		ajax::success($ret);
 	} elseif (init('action') == 'reDiscover') {
