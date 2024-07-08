511b302f7cc558d580da7fc68cec1e0cbe607c5e
Nebz <28622481+NebzHB@users.noreply.github.com>
2024-07-08 17:04:05 +0200

    Update configuration.php

--- plugin_info/configuration.php
+++ plugin_info/configuration.php
@@ -43,7 +43,7 @@ pre#pre_eventlog {
 		</legend>
 		<div class="form-group">
 			<label class="col-lg-6 control-label help" data-help="{{Période en miliseconde pour interroger l'équipement, il est déconseillé par le constructeur de descendre sous 1 seconde}}">{{Période}} HWE-SKT</label>
-			<div class="col-lg-2">
+			<div class="col-lg-1">
 				<div class="input-group">
 					<input class="configKey form-control roundedLeft" data-l1key="period_HWE-SKT" placeholder="5000" /><span class="input-group-addon roundedRight">ms</span>
 				</div>
