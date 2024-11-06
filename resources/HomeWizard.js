5260f9802b091de7c1cc0da8747dd97d15af5ffc
Nebz <28622481+NebzHB@users.noreply.github.com>
2024-11-06 07:46:33 +0100

    Update HomeWizard.js

--- resources/HomeWizard.js
+++ resources/HomeWizard.js
@@ -314,7 +314,7 @@ function discover() {
 				discovery.stop();
 				setTimeout(() => {
 					Logger.log(index+' (getData) : Starting Discovery...',LogType.DEBUG);
-					discovery.start();
+				    discover();
 				},5000);
 			}
 		});
